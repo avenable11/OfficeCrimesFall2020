d0bd36c037b21874a3e58bf76a124f8f4abb8b85
Angela K Venable <avenable11@ivytech.edu>
2020-12-03 20:15:47 -0500

    Added DatePicker dialog

--- app/src/main/java/edu/ivytech/officecrimesfall2020/CrimeDetailFragment.java
+++ app/src/main/java/edu/ivytech/officecrimesfall2020/CrimeDetailFragment.java
@@ -1,5 +1,7 @@
 package edu.ivytech.officecrimesfall2020;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,17 +16,21 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static android.widget.CompoundButton.*;
 
 public class CrimeDetailFragment extends Fragment {
     private static final String ARG_CRIME_ID = "edu.ivytech.officecrimesfall2020.crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckbox;
+    private static final int REQUEST_DATE = 0;
 
     public static CrimeDetailFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -69,8 +75,18 @@ public class CrimeDetailFragment extends Fragment {
             }
         });
         mDateButton = v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        updateDate();
+        //mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getParentFragmentManager();
+               // DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeDetailFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
         mSolvedCheckbox = v.findViewById(R.id.crime_solved);
         mSolvedCheckbox.setChecked(mCrime.isSolved());
         mSolvedCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
@@ -80,6 +96,22 @@ public class CrimeDetailFragment extends Fragment {
             }
         });
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if(requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -100,4 +132,6 @@ public class CrimeDetailFragment extends Fragment {
        crimeLab.updateCrime(mCrime);
         super.onPause();
     }
+
+
 }
---
+++ app/src/main/java/edu/ivytech/officecrimesfall2020/DatePickerFragment.java
@@ -0,0 +1,79 @@
+package edu.ivytech.officecrimesfall2020;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DatePickerFragment extends DialogFragment {
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+    public static final String EXTRA_DATE = DatePickerFragment.class.getSimpleName() + ".date";
+
+    public static DatePickerFragment newInstance(Date date) {
+       Bundle args = new Bundle();
+       args.putSerializable(ARG_DATE, date);
+       DatePickerFragment fragment = new DatePickerFragment();
+       fragment.setArguments(args);
+       return fragment;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+        mDatePicker = v.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
+        AlertDialog a = new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        int year = mDatePicker.getYear();
+                        int month = mDatePicker.getMonth();
+                        int day = mDatePicker.getDayOfMonth();
+                        Date date = new GregorianCalendar(year, month, day).getTime();
+                        sendResult(Activity.RESULT_OK, date);
+                    }
+                })
+                .create();
+        Window window = a.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.CENTER_HORIZONTAL;
+        window.setAttributes(wlp);
+        return a;
+    }
+
+    private void sendResult(int resultCode, Date date) {
+        if(getTargetFragment() == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, date);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+    }
+}
